771a7bdd46f6a69eeb75e45aa663c660a039b6a0
mariarpires <maria.a.pires@aluno.senai.br>
2025-01-29 11:16:58 -0300

    listas

---
+++ listas/lista1.js
@@ -0,0 +1,51 @@
+// console.log("Olá, mundo!")
+//atv 1
+
+let a = 24
+let b = 8
+
+let soma = a + b
+let sub = a - b
+let mult = a * b
+let div = a / b
+let restodiv = a % b
+
+console.log(soma)
+console.log(sub)
+console.log(mult)
+console.log(div)
+console.log(restodiv)
+
+//atv2
+
+let h = 10
+let l = 3
+let area = h*l
+console.log(area)
+
+//atv3
+
+let c = 18
+
+let dobro = c*2
+let metade = c/2
+
+console.log(dobro)
+console.log(metade)
+
+
+//atv4
+
+let d = 8
+let e = 7
+let f = 9
+
+let media = (d + e + f) / 3
+console.log(media)
+
+//atv5
+
+let celsius = 18
+
+let fahrenheit = (celsius * 9/5) + 32
+console.log(fahrenheit)
