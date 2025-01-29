6b69ac878e4f414288531117b20210b943ecfc80
mariarpires <maria.a.pires@aluno.senai.br>
2025-01-29 15:33:48 -0300

    Update lista2.js

---
+++ listas/lista2.js
@@ -0,0 +1,50 @@
+//atv1
+
+let n1 = 18
+let n2 = 21
+
+let maior = n1 > n2
+let menor = n1 < n2
+let igual = n1 == n2
+
+console.log(maior)
+console.log(menor)
+console.log(igual)
+
+//atv2
+
+let i = 72
+
+let pode = i >= 16
+let obrigado = i >= 18 && i <=70
+console.log(pode)
+console.log(obrigado)
+
+//atv3
+
+let idade = 72
+
+let maiorr = i >= 18
+let idoso = i >= 60
+console.log(pode)
+console.log(obrigado)
+
+//atv4
+
+let user = maria == admin
+console.log(user)
+
+//atv5
+
+//atv6
+
+let sd = 2000
+let sq = 500
+
+let s = sd - sq 
+
+let apos = s >= 100
+
+console.log(sd)
+console.log(sq)
+console.log(s)
