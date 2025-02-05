165de34b79dc778018b64c18354959a1e5c30c1d
mariarpires <maria.a.pires@aluno.senai.br>
2025-02-05 11:47:05 -0300

    lista6

---
+++ lista6.js
@@ -0,0 +1,77 @@
+//atv1
+let dia = prompt("Qual dia da semana?")
+if (dia === 1) {
+    console.log("Domingo")
+} else if (dia == 2) {
+    console.log("Segunda-feira")
+} else if (dia == 3) {
+    console.log("Terça-feira")
+} else if (dia == 4) {
+    console.log("Quarta-feira")
+} else if (dia == 5) {
+    console.log("Quinta-feira")
+} else if (dia == 6) {
+    console.log("Sexta-feira")
+} else if (dia == 7) {
+    console.log("Sábado")
+} else {
+    console.log("Número inválido. Insira um valor entre 1 e 7")
+}
+console.log("-----------------")
+//atv2
+let idade = prompt("Qual sua idade?")
+if (idade == 5) {
+    console.log("Infantil A")
+} else if (idade == 10) {
+    console.log("Infantil B")
+} else if (idade == 15) {
+    console.log("Juvenil A")
+} else if (idade == 20) {
+    console.log("Juvenil B")
+} else if (idade == 30) {
+    console.log("Adulto")
+} else {
+    console.log("Idade inválida. Insira 5, 10, 15, 20 ou 30")
+}
+console.log("-----------------")
+
+//atv3
+let turno = prompt("Qual seu turno da escola? (sigla)")
+if (turno == "M") {
+    console.log("Bom dia!")
+} else if (turno == "V") {
+    console.log("Boa tarde!")
+} else if (turno == "N") {
+    console.log("Boa noite!")
+} else {
+    console.log("Turno inválido. Insira M, V ou N")
+}
+console.log("-----------------")
+//atv4
+let num = prompt("Digite um número de 1 a 5")
+if (num === 1) {
+    console.log("Oii!")
+} else if (num == 2) {
+    console.log("Bons estudos!")
+} else if (num == 3) {
+    console.log("Boa trabalho!")
+} else if (num == 4) {
+    console.log("Boa refeição!")
+} else if (num == 5) {
+    console.log("Tchau!")
+} else {
+    console.log("Número fora do intervalo. Insira um valor entre 1 e 5.")
+}
+//atv5
+let estacao = prompt("Insira um número de 1 a 4")
+if (dia === 1) {
+    console.log("Primavera")
+} else if (dia == 2) {
+    console.log("Verão")
+} else if (dia == 3) {
+    console.log("Outono")
+} else if (dia == 4) {
+    console.log("Inverno")
+} else {
+    console.log("Estação inválida. Insira um número de 1 a 4.")
+}
